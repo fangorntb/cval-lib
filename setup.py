a56ee119c9b09dd02b12211df7848fc3989d465f
fangorntreabeard <cyrillbelyakov@vk.com>
2023-09-04 03:22:52 +0700

    - updating version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='cval-lib',
-    version='0.0.2.29',
+    version='0.0.2.31',
     description='python computer vision active learning library',
     author='DGQ | Cyrill Belyakov',
     author_email='',
