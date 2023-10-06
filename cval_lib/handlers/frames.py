f5f8b9718a4186c5b2649ebc7a54e44f9b69d690
superuser <cyrillbelyakov@vk.com>
2023-10-06 15:36:12 +0700

    bug fix

--- cval_lib/handlers/frames.py
+++ cval_lib/handlers/frames.py
@@ -24,12 +24,21 @@ class Frames(AbstractHandler):
     def _validate(frames: Iterable[FrameModel]):
         return list(map(lambda x: x.dict(), filter(lambda x: x.img_link is not None, frames))) if frames else None
 
-    def read_meta(self):
+    def read_meta(self, limit: int = 100):
         if self.part_of_dataset is not None:
-            return (self._get(self.route + f'{self.part_of_dataset}/frames/meta', stream=True), self.send())[1]
+            return (
+                self._get(
+                    self.route + f'{self.part_of_dataset}/frames/meta',
+                    stream=True,
+                    params={'limit': limit},
+                ), self.send())[-1]
         else:
             return [
-                (self._get(self.route + f'{part_of_dataset}/frames/meta', stream=True), self.send())[1]
+                (self._get(
+                    self.route + f'{part_of_dataset}/frames/meta',
+                    stream=True,
+                    params={'limit': limit},
+                ), self.send().json())[-1]
                 for part_of_dataset in ('test', 'training', 'validation')
             ]
 
@@ -39,14 +48,16 @@ class Frames(AbstractHandler):
         Based on: FrameModel(img_raw: bytes, img_external_id: str)
         :param frames: Image values in bytes
         """
+        print(tuple(map(lambda x: ('uploaded_files', x.img_raw), frames))[0])
         self._post(
             self.route + f'{self.part_of_dataset}/blob',
-            files=tuple(map(lambda x: x.img_raw, frames)),
+            files=tuple(map(lambda x: ('uploaded_files', x.img_raw), frames)),
             params={'frames_ids': ','.join(list(map(lambda x: x.img_external_id, frames)))},
         )
         return self.send()
 
-    def create_fl(self, train: Iterable[FrameModel] = None, val: Iterable[FrameModel] = None, test: Iterable[FrameModel] = None):
+    def create_fl(self, train: Iterable[FrameModel] = None, val: Iterable[FrameModel] = None,
+                  test: Iterable[FrameModel] = None):
         """
         uploading images via links
         Based on: FrameModel(img_link: str, img_external_id: str)
@@ -55,10 +66,10 @@ class Frames(AbstractHandler):
         :param test: validation part of dataset
         """
         self._post(
-            self.route + f'frames/linked',
+            self.route + 'frames/linked',
             json={
                 'train_frames_links': self._validate(train),
-                'test_frames_links':  self._validate(test),
+                'test_frames_links': self._validate(test),
                 'val_frames_links': self._validate(val),
             },
         )
